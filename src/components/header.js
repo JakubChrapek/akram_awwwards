31dcd7680a790533e6f5df9ea8d2f6e99970dd19
JakubChrapek <chrapekuba@gmail.com>
2020-06-18 15:19:01 +0200

    Global styles, theme and header component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// styled components
+import { HeaderNav } from '../styles/headerStyles';
+
+const Header = () => {
+  return (
+    <HeaderNav>
+      header
+    </HeaderNav>
+  )
+}
+
+export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,35 @@ import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+// styled components
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+
+// components
+import Header from './header';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  * {
+    text-decoration: none;
+    /* cursor: none; */
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden;
+  }
+`
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -13,7 +42,23 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return <main>{children}</main>
+  const darkTheme = {
+    background: '#000',
+    text: '#fff'
+  }
+
+  const lightTheme = {
+    background: '#fff',
+    text: '#000'
+  }
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <Header />
+      <main>{children}</main>
+    </ThemeProvider>
+  )
 }
 
 Layout.propTypes = {
